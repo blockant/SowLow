159c4ccd10448995eb4b473eb59b43742522a909
Akshay Jain <akshayjain1999.aj18@gmail.com>
2021-12-26 15:36:42 +0530

    feat(user): Added User Profile and Edit Profile Functionality

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,1 +1,45 @@
 import User from "../models/User";
+import { Request, Response } from "express";
+import Logger from "../providers/Logger";
+class Users{
+    public static async editUser(req: Request, res: Response){
+        try {
+            const {name}=req.body
+            const foundUser= await User.findById(res.locals.userId)
+            if(!foundUser){
+                throw new Error('User Not Found')
+            }
+            if(name){
+                foundUser.name=name
+            }
+            await foundUser.save()
+            return res.status(200).json({message: 'User Updated Success', user: foundUser})
+        } catch (err) {
+            Logger.error(err)
+            return res.status(500).json({message: 'Server Error', error: err.message})
+        }
+    }
+    public static async getUserById(req: Request, res: Response){
+        try{
+            const {userId}=req.params
+            const foundUser= await User.findById(userId)
+            if(!foundUser){
+                throw new Error('User Not Found')
+            }
+            return res.status(200).json({message: 'Found User Is', user: foundUser})
+        }catch(err){
+            Logger.error(err)
+            return res.status(500).json({message: 'Server Error', error: err.message})
+        }
+    }
+    public static async getLoggedInUser(req: Request, res: Response){
+        try{
+            const foundUser=await User.findById(res.locals.userId)
+            return res.status(200).json({mesage: 'Succes', user: foundUser})
+        }catch(err){
+            Logger.error(JSON.stringify(err))
+            return res.status(500).json({message: 'Server Error', error: err.message})
+        }
+    }
+}
+export default Users
---
+++ src/middlewares/Auth.ts
@@ -0,0 +1,19 @@
+// The Following middleware Checks if current user is logged in Or Not
+import { Request, Response, NextFunction } from "express";
+import JWT from "../providers/JWT";
+import Logger from "../providers/Logger";
+import jwt from 'jsonwebtoken'
+import Locals from "../providers/Locals";
+export const isLoggedIn= (req: Request, res: Response, next: NextFunction)=>{
+    try{
+        if(!req.headers.authorization){
+            throw new Error('No Token Sent')
+        }
+        const decoded = jwt.verify(JWT.getToken(req), Locals.config().jwtSecretKey);
+        res.locals.userId=decoded.sub
+        next()
+    }catch(err){
+        Logger.error(JSON.stringify(err))
+        return res.status(500).json({message: 'Server Error', error: err.message})
+    }
+}
